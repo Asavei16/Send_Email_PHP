b95e7e161f49ed3ea0602e8e846b9d0c0a08b1bb
Florin Asavei <57408487+Asavei16@users.noreply.github.com>
2024-03-18 11:55:36 +0200

    Add files via upload

--- email.php
+++ email.php
@@ -1,9 +1,9 @@
 <?php
 
-$to_email="florin.asavei@autonom.com";
+$to_email="user@gmail.com";
 $subject = "Send Email via PHP";
 $body = "Salut, acesta este un test trimis prin PHP Script";
-$headers = "From: asaveiflo@gmail.com";
+$headers = "From: user1@gmail.com";
 
 if(mail($to_email, $subject, $body, $headers)) {
     echo "Email successfully sent to " . $to_email."...";
